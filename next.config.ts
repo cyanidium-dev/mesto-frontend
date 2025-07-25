5f8a705c51421cf61b952e0552d611dcbc7b9d05
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-07-25 18:28:40 +0300

    updated next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+const withPWA = require("next-pwa")({
+  dest: "public", // де буде створено service-worker і manifest
+  register: true, // автоматична реєстрація SW
+  skipWaiting: true, // SW одразу активується без очікування
+  disable: process.env.NODE_ENV === "development", // вимкнути PWA в dev
+  buildExcludes: [/middleware-manifest\.json$/], // уникнути помилок з middleware
+});
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = withPWA({
+  reactStrictMode: true,
+});
