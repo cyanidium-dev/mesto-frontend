5d9d99d422cf06b8336a4d68f9742429ace201da
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-07-28 15:02:04 +0300

    added prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-import MainButton from "@/components/shared/buttons/MainButton";
-import ProgressBar from "@/components/shared/progress/ProgressBar";
-import SectionTitle from "@/components/shared/titles/SectionTitle";
-
 export default function Home() {
   return <></>;
 }
