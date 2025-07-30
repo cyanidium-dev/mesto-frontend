9040cc7fef9012f186fe9e2cac5d4441149b893a
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-07-30 17:26:29 +0300

    added basic map component

--- src/components/mainPage/Main.tsx
+++ src/components/mainPage/Main.tsx
@@ -1,14 +1,59 @@
 "use client";
-import { useState } from "react";
-import Container from "../shared/container/Container";
+
+import { useEffect, useState } from "react";
 import SearchBar from "./SearchBar";
+import dynamic from "next/dynamic";
+import List from "./List";
+
+const Map = dynamic(() => import("./Map"), {
+  ssr: false, // виключає серверний рендеринг для цього компонента
+});
 
 export default function Main() {
   const [viewMode, setViewMode] = useState<"map" | "list">("map");
+  const [userLocation, setUserLocation] = useState<[number, number] | null>(
+    null
+  );
+  const [mapCenter, setMapCenter] = useState<[number, number]>([
+    41.5463, 2.1086,
+  ]);
+
+  // Отримати геолокацію при першому завантаженні, встановити userLocation і лише якщо mapCenter дефолтний — оновити його
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((pos) => {
+        const coords: [number, number] = [
+          pos.coords.latitude,
+          pos.coords.longitude,
+        ];
+        setUserLocation(coords);
+
+        setMapCenter((currentCenter) => {
+          // Якщо центр карти ще дефолтний, оновлюємо на userLocation
+          // Інакше залишаємо як є (щоб не скидати, якщо користувач уже змінив центр)
+          const defaultCenter = [41.5463, 2.1086];
+          const isDefaultCenter =
+            currentCenter[0] === defaultCenter[0] &&
+            currentCenter[1] === defaultCenter[1];
+
+          return isDefaultCenter ? coords : currentCenter;
+        });
+      });
+    }
+  }, []);
 
   return (
-    <Container className="min-h-screen">
+    <>
       <SearchBar viewMode={viewMode} setViewMode={setViewMode} />
-    </Container>
+      {viewMode === "map" ? (
+        <Map
+          center={mapCenter}
+          onCenterChange={setMapCenter}
+          userLocation={userLocation}
+        />
+      ) : (
+        <List />
+      )}
+    </>
   );
 }
